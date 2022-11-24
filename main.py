306665f8a3397fbfe25775e41922682997043293
OneWinterSigh <40102955+OneWinterSigh@users.noreply.github.com>
2022-11-24 11:21:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from openpyxl import Workbook
 from datetime import datetime
-import tkinter as tk
-from tkinter import ttk
-
 
 class PersonaHorario:
     def __init__(self, nombre, codigo):
@@ -108,46 +105,6 @@ class Funcionando:
 
         self.pintarExcel()
 
-"""
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title('Tkinter StringVar')
-        self.geometry("500x500+200+200")
 
-        self.name_var = tk.StringVar()
-        self.horaentrada_var = tk.StringVar()
-        self.horasalida_var = tk.StringVar() 
-
-        self.columnconfigure(0, weight=1)
-        self.columnconfigure(1, weight=1)
-        self.columnconfigure(2, weight=1)
-
-        self.create_widgets()
-
-    def create_widgets(self):
-
-        padding = {'padx': 5, 'pady': 5}
-        # label
-        ttk.Label(self, text='Introduce código:').grid(column=0, row=0, **padding)
-
-        # Entry
-        name_entry = ttk.Entry(self, textvariable=self.name_var)
-        name_entry.grid(column=1, row=0, **padding)
-        name_entry.focus()
-
-        # Button
-        submit_button = ttk.Button(self, text='Submit', command=self.submit)
-        submit_button.grid(column=2, row=0, **padding)
-
-        # Output label
-        self.output_label = ttk.Label(self)
-        self.output_label.grid(column=0, row=1, columnspan=3, **padding)
-
-    def submit(self):
-        self.output_label.config(text=self.name_var.get())
-
-"""
 if __name__ == "__main__":
     app = App()
-    app.mainloop()
